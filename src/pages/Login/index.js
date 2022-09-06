21c91458c7d256aaa3872e0cdc7b40642959fbdb
Leonardo <leojorigo@gmail.com>
2022-09-06 16:57:05 -0300

    adicao das paginas de registro e login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,9 +3,16 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>
-      TELA LOGIN
-    </h1>
+    <div class="container">
+      <p>Faca seu login</p>
+       <form>
+        <label>Informe seu e-mail</label>
+        <input type="text" id="lemail" name="lemail"></input>
+        <label>Informe sua senha</label>
+        <input type="text" id="lpassword" name="lpassword"></input>
+      </form>
+      <button class="waves-effect waves-light btn"> Login </button>
+    </div>
   );
 }
 
--- src/pages/Register_page/index.js
+++ src/pages/Register_page/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>
-      <h1>TELA CADASTRO</h1>
-    </div>
+    <div class="container">
+      <form>
+        <label>Seu e-mail</label>
+        <input type="text" id="remail" name="remail"></input>
+        <label>Informe seu nome</label>
+        <input type="text" id="rfname" name="rfname"></input>
+        <label>Agora seu sobrenome</label>
+        <input type="text" id="rlname" name="rlname"></input>
+        <label>Crie uma senha</label>
+        <input type="text" id="password" name="password"></input>
+        <label>Confirme sua senha</label>
+        <input type="text" id="cpassword" name="cpassword"></input>
+      </form>
+      <button type="submit" class="waves-effect waves-light btn"> Cadastrar </button>
+  </div>
   );
 }
 
