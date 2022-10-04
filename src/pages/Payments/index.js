abca4b98ec227c8240fba5e4482f3c5880344d36
Leonardo <leojorigo@gmail.com>
2022-10-03 23:57:10 -0300

    primeira integracao api

--- src/pages/Payments/index.js
+++ src/pages/Payments/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Sidebar from '../../components/sidebar';
 import Navbar from '../../components/navbar';
+import axios from 'axios';
+import { AuthContext } from '../../contexts/authContext';
+import M from "materialize-css";
 
 function Payments() {
+
+  const { token, setToken } = useContext(AuthContext);
+  const [payments, setProfile] = useState({});
+
+  useEffect(() => {
+    M.AutoInit();
+
+    const headers = {
+        'Authorization': `Bearer ${token}`
+    }
+
+    axios.get('http://localhost:8080/user', { headers })
+        .then(res => setProfile(res.data))
+        .catch(error => console.error(error));
+}, []);
+
   return (
     <>
       <Navbar/>
@@ -15,9 +34,17 @@ function Payments() {
       <div className="container row">
         <div className="col s12">
           <ul className="tabs">
-            <li className="tab col s3"><a href=" " className="black-text">Contas à Pagar</a></li>
-            <li className="tab col s3"><a href=" " className="black-text">Pagamentos Recebidos</a></li>
+            <li className="tab col s3"><a href="#test1" className="black-text">Pagamentos a Receber</a></li>
+            <li className="tab col s3"><a href="#test2" className="black-text">Pagamentos Recebidos</a></li>
           </ul>
+          <div id="test1" class="col s12">
+            <span>Assinatura: </span>
+            <span>{payments.name}</span>
+            <br></br>
+            <span>Nome: </span>
+            <span>{payments.firstName}</span>
+          </div>
+          <div id="test2" class="col s12">Test 1</div>
         </div>
       </div>
     </>
