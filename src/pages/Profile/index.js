9cf1ffd37739710d65fbb97f7eaa88d570ca9883
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-10-03 22:48:24 -0300

    Finish profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Sidebar from '../../components/sidebar';
 import Navbar from '../../components/navbar';
 import axios from 'axios';
+import { AuthContext } from '../../contexts/authContext';
+import M from "materialize-css";
 
 function Profile() {
 
-  const handleUserData = async () => {
-    axios.get('http://localhost:8080/user')
-      .then(res => console.log(res.data)).catch(error => console.error(error));
-  }
+    const { token, setToken } = useContext(AuthContext);
+    const [profile, setProfile] = useState({});
 
-  return (
-    <>
-      <Navbar />
-      <Sidebar />
-      <br></br>
-      <br></br>
-      <div className='container row'>
-        <h3 className="title">PERFIL  </h3>
-      </div>
-      <div className='container row'><button className='btn-large' onClick={handleUserData}>Ver Dados do Perfil</button></div>
-    </>
-  );
+    useEffect(() => {
+        M.AutoInit();
+
+        const headers = {
+            'Authorization': `Bearer ${token}`
+        }
+
+        axios.get('http://localhost:8080/user', { headers })
+            .then(res => setProfile(res.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <>
+            <Navbar />
+            <Sidebar />
+            <br></br>
+            <br></br>
+            <div className='container row'>
+                <ul class="collection with-header">
+                    <li class="collection-header"><h3>Perfil</h3></li>
+                    <li class="collection-item">
+                        <h5>Nome:</h5>
+                        <p>{profile.fullName}</p>
+                    </li>
+                    <li class="collection-item">
+                        <h5>E-mail:</h5>
+                        <p>{profile.email}</p>
+                    </li>
+                </ul>
+            </div>
+
+        </>
+    );
 }
 
 export default Profile;
